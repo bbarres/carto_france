327f4772461d7a467336cf5fc510b3de1062e3e8
bbarres <benoit.barres@gmail.com>
2018-12-02 15:27:57 +0100

    Adpating the code for the new data organization

--- carto_base.R
+++ carto_base.R
@@ -6,48 +6,76 @@
 
 #loading the packages necessary for the analysis
 library(rgdal)
+library(rgeos)
 library(plotrix)
 library(classInt)
 
-#Setting the right working directory
-setwd("~/work/Rfichiers/Githuber/carto_france")
-
 
 ###############################################################################
-#loading the data
+#loading the data and producing lighter dataset
 ###############################################################################
 
 #the geographical layer used here were downloaded on the IGN website (Geofla), 
 #which is the French Institut of Geographic and forest information
 
 #the smallest administrative unit in France: communes
-commu<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
+commu<-readOGR(dsn="C:/Users/benoi/OneDrive/Rfichiers/carto_france/data/ADE_1-1_SHP_LAMB93_FR",
                layer="COMMUNE")
+save(commu,file="output/commu.RData")
+commuLight<-gSimplify(commu,1000,topologyPreserve=TRUE)
+save(commuLight,file="output/commuLight.RData")
+load("output/commuLight.RData")
+plot(commuLight)
+
+#EPCI means "Etablissements Publics de Coopération Intercommunale"
+EPCI<-readOGR(dsn="C:/Users/benoi/OneDrive/Rfichiers/carto_france/data/ADE_1-1_SHP_LAMB93_FR",
+              layer="EPCI")
+save(EPCI,file="output/EPCI.RData")
+EPCILight<-gSimplify(EPCI,1000,topologyPreserve=TRUE)
+save(EPCILight,file="output/EPCILight.RData")
+load("output/EPCILight.RData")
+plot(EPCILight)
+
+#arrondissement are an intermediate administrative division
+arrond<-readOGR(dsn="C:/Users/benoi/OneDrive/Rfichiers/carto_france/data/ADE_1-1_SHP_LAMB93_FR",
+                layer="ARRONDISSEMENT_DEPARTEMENTAL")
+save(arrond,file="output/arrond.RData")
+arrondLight<-gSimplify(arrond,1000,topologyPreserve=TRUE)
+save(arrondLight,file="output/arrondLight.RData")
+load("output/arrondLight.RData")
+plot(arrondLight)
+
+#département are an intermediate administrative division
+departe<-readOGR(dsn="C:/Users/benoi/OneDrive/Rfichiers/carto_france/data/ADE_1-1_SHP_LAMB93_FR",
+                 layer="DEPARTEMENT")
+save(departe,file="output/departe.RData")
+departeLight<-gSimplify(departe,1000,topologyPreserve=TRUE)
+save(departeLight,file="output/departeLight.RData")
+load("output/departeLight.RData")
+plot(departeLight)
+
+#régions are the largest administrative division in France
+regions<-readOGR(dsn="C:/Users/benoi/OneDrive/Rfichiers/carto_france/data/ADE_1-1_SHP_LAMB93_FR",
+                 layer="REGION")
+save(regions,file="output/regions.RData")
+regionsLight<-gSimplify(regions,1000,topologyPreserve=TRUE)
+save(regionsLight,file="output/regionsLight.RData")
+load("output/regionsLight.RData")
+plot(regionsLight)
+
+
+###############################################################################
+#some example of geographic data manipulation
+###############################################################################
+
+
+#some information on the data structure of the geodata
 class(commu)
 slotNames(commu)
 summary(commu@data)
-plot(commu)
 
-#EPCI means "Etablissements Publics de Coopération Intercommunale"
-EPCI<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
-              layer="EPCI")
-plot(EPCI)
-
-#arrondissement are an intermediate
-arrond<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
-                layer="ARRONDISSEMENT_DEPARTEMENTAL")
-plot(arrond)
-
-departe<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
-                 layer="DEPARTEMENT")
-plot(departe)
-
-
-regions<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
-                 layer="REGION")
-plot(regions)
-
-op<-par(mar=c(0,0,0,0))
+#some plotting examples
+op<-par(mar=c(0,0,0,0),mfrow=c(2,2))
 plot(EPCI,border="grey60",lwd=0.1)
 plot(departe,add=TRUE,border="darkred",lwd=0.5)
 plot(regions,add=TRUE,lwd=1.5)
