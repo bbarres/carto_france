87a1c9a9f7bf4f642295f503f9ccbfdcc78b3e6b
bbarres <benoit.barres@gmail.com>
2021-01-30 23:58:10 +0100

    adding session info to France map code

--- carto_base.R
+++ carto_base.R
@@ -167,5 +167,16 @@ text(NomDepSP,labels=NomDepSP$depID)
 
 
 ##############################################################################/
+#Writing info session for reproducibility####
+##############################################################################/
+
+sink("session_info.txt")
+print(sessioninfo::session_info())
+sink()
+#inspired by an R gist of François Briatte: 
+#https://gist.github.com/briatte/14e47fb0cfb8801f25c889edea3fcd9b
+
+
+##############################################################################/
 #END
 ##############################################################################/
